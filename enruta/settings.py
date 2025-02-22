b4d7a92ac7faaf4f14ccdc9d6a6818f70a172fa8
bradsguaso <a.bradshawf07@gmail.com>
2025-02-22 12:40:48 -0500

    todos los servicios con autenticacion JWT

--- enruta/settings.py
+++ enruta/settings.py
@@ -139,7 +139,9 @@ REST_FRAMEWORK = {
     ],'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
+    ), 'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
 }
 
 
